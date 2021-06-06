a89166b39fc295ec235cc08383a34b280eae86f7
Cristiano Andrade <cristianocontabil@gmail.com>
2021-06-06 19:04:20 -0300

    fsdgfasgdf

---
+++ Loja.DTO/usuario_DTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.DTO
+{
+    /*
+    * DTO
+    * É a camada em que estão localizados os objetos e as tabelas.
+    * Esta camada não faz referência a ninguém
+    */
+
+    public class usuario_DTO
+    {
+        public int cod_usuario { get; set; }
+        public string nome { get; set; }
+        public string login { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public DateTime cadastro { get; set; }
+        public string situacao { get; set; }
+        public int perfil { get; set; }
+
+
+    }
+}
--- Loja/MDIParent1.cs
+++ Loja/MDIParent1.cs
@@ -12,6 +12,16 @@ namespace Loja
 {
     public partial class MDIParent1 : Form
     {
+        /*
+         * Responsável pela interação com os usuários. Nessa camada, temos a Interface – Forms.
+         * 
+         * UI
+         * User Interface
+         * 
+         * É a camada responsável pela interação com os usuários, onde temos a Interface (Forms).
+         * Faz referência às camadas BLL e DTO.         
+         */
+
         private int childFormNumber = 0;
 
         public MDIParent1()
@@ -19,89 +29,17 @@ namespace Loja
             InitializeComponent();
         }
 
-        private void ShowNewForm(object sender, EventArgs e)
+        private void usuáriosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Form childForm = new Form();
+            /*
+             * Chamada da form Cadastro usuario dentro da MDI
+             * ou seja, abrir dentro do form principal
+             * chama-se a janela filha(childForm) dentro do form principal
+             */
+
+            Form childForm = new Cadastro_usuario();
             childForm.MdiParent = this;
-            childForm.Text = "Janela " + childFormNumber++;
             childForm.Show();
-        }
-
-        private void OpenFile(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            openFileDialog.Filter = "Arquivos de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
-            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = openFileDialog.FileName;
-            }
-        }
-
-        private void SaveAsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-            saveFileDialog.Filter = "Arquivos de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
-            if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
-            {
-                string FileName = saveFileDialog.FileName;
-            }
-        }
-
-        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
-        }
-
-        private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
-        }
-
-        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.Cascade);
-        }
-
-        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileVertical);
-        }
-
-        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileHorizontal);
-        }
-
-        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.ArrangeIcons);
-        }
-
-        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            foreach (Form childForm in MdiChildren)
-            {
-                childForm.Close();
-            }
         }
     }
 }
